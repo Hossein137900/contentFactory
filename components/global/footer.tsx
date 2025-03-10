94baa14b0cf28b95c9fcf7e0e1aea7453a48692e
Hossein <141348426+SHEKAF1234@users.noreply.github.com>
2025-03-10 17:58:39 +0330

    wdwdwd

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, HTMLMotionProps } from "framer-motion";
 import { useState } from "react";
 import {
   FaGithub,
