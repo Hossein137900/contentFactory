65185b645e31cc2c38bdc00c4511939409655120
MacBook <mohammad767689@gmail.com>
2025-03-11 16:23:10 +0330

    push koloft

---
+++ models/request.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+const requestSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+export default mongoose.models.Request || mongoose.model("Request", requestSchema);
